de31620795109256c2b93398b7aed19d4a5fed85
cburzo1 <gm.c.burzo@gmail.com>
2025-10-02 14:15:35 -0400

    added get max depth

--- LL_Trees_Graphs/Trees/Binary_Tree.py
+++ LL_Trees_Graphs/Trees/Binary_Tree.py
@@ -60,10 +60,48 @@ class BinaryTree:
         print(p.data)
         self.in_order(p.rc)
 
+    def get_max_depth(self, p):
+        if p is None:
+                return -1
+
+        # compute the height of left and right subtrees
+        lHeight = self.get_max_depth(p.lc)
+        rHeight = self.get_max_depth(p.rc)
+
+        return max(lHeight, rHeight) + 1
+
+    def level_order(self, p):
+
+        Q = []
+
+        if p is not None:
+            print(p.data)
+            Q.append(p)
+
+        while len(Q) > 0:
+            dq = Q.pop(0)
+
+            if dq.lc is not None:
+                print(dq.lc.data)
+                Q.append(dq.lc)
+
+            if dq.rc is not None:
+                print(dq.rc.data)
+                Q.append(dq.rc)
+
 bt = BinaryTree()
 
-bt.createBinaryTree(4)
-bt.createBinaryTree(2)
-bt.createBinaryTree(5)
+bt.createBinaryTree('a')
+bt.createBinaryTree('b')
+bt.createBinaryTree('c')
+bt.createBinaryTree('d')
+bt.createBinaryTree('e')
+bt.createBinaryTree(None)
+bt.createBinaryTree(None)
+bt.createBinaryTree(None)
+bt.createBinaryTree(None)
+bt.createBinaryTree('f')
+bt.createBinaryTree(None)
 
-bt.in_order(bt.root)
+bt.pre_order(bt.root)
+print(bt.get_max_depth(bt.root))
