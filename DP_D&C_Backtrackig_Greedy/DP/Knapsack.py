c7cc52d6808bfd484c0abcc3f15ad59fd91b2b87
cburzo1 <gm.c.burzo@gmail.com>
2025-10-03 12:49:16 -0400

    completed sets review for knapsack

--- DP_D&C_Backtrackig_Greedy/DP/Knapsack.py
+++ DP_D&C_Backtrackig_Greedy/DP/Knapsack.py
@@ -9,7 +9,7 @@ objects in our knapsack such that we get the highest amount of profit
 for what the bag can hold in total weight. We want to maximize profit
 so this is an optimization problem.
 
-x={0,1,1,0} <- this is how we denot what is included in the bag and
+x={0,1,1,0} <- this is how we denote what is included in the bag and
 what is not included. In greedy we are taking fractions of the object.
 here we either take the object or we don't.
 '''
@@ -19,5 +19,19 @@ Sets method:
 ------------
 m=8 P={1,2,5,6}
 n=4 W={2,3,4,5}
-    x={0,1,1,0}
+    x={0, 1, 1, 0}
+       x1 x2 x3 x4
+
+permutations: x1 x2 x3 x4
+0000, 0001, 0010, 0100 ... 2^4 possible solutions
+
+review probability: each element can be either 1 or 0, that is
+2 * 2 * 2 * 2 gives us the permutations r^k because repetitions
+are acceptable.
+
+O(2^n) algorithms solve this
+
+* see png for notes and steps:
+
 '''
+
