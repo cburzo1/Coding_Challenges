632f510374fc01e9b912d393399edb3828551af3
cburzo1 <gm.c.burzo@gmail.com>
2025-11-20 07:07:02 -0500

    completed tries

---
+++ LL_Trees_Graphs/Trees/trie.py
@@ -0,0 +1,42 @@
+class TrieNode:
+    def __init__(self):
+        self.endOfWord = False
+        self.children = {}
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+
+        return cur.endOfWord
+
+    def startswith(self, prefix: str) -> bool:
+        cur = self.root
+
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return True
+
+T = Trie()
+
+T.insert("cat")
+T.insert("cattle")
+print(T.search("cattle"))
